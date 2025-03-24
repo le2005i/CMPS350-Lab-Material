2cd5e5b3c3f0816cb51626414b86dcfcf4344062
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 15:59:39 +0300

    up

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
@@ -3,27 +3,31 @@ import React from 'react'
 export default function AccountsTable({ initialAccounts }) {
     return (
         <table id="accounts">
-            <tr>
-                <th>Account No</th>
-                <th>Account Type</th>
-                <th>Balance</th>
-                <th>Action</th>
-            </tr>
-            {initialAccounts.map(acct =>
-                <tr id="row-{acct.accountNo}">
-                    <td>{acct.accountNo}</td>
-                    <td>{acct.acctType}</td>
-                    <td>{acct.balance}</td>
-                    <td>
-                        {acct.balance == 0 ?
-                            <button onclick="handleDeleteAccount('${acct.accountNo}')">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                            : ''
-                        }
-                    </td>
+            <thead>
+                <tr>
+                    <th>Account No</th>
+                    <th>Account Type</th>
+                    <th>Balance</th>
+                    <th>Action</th>
                 </tr>
-            )}
+            </thead>
+            <tbody>
+                {initialAccounts.map(acct =>
+                    <tr id="row-{acct.accountNo}">
+                        <td>{acct.accountNo}</td>
+                        <td>{acct.acctType}</td>
+                        <td>{acct.balance}</td>
+                        <td>
+                            {acct.balance == 0 ?
+                                <button onclick="handleDeleteAccount('${acct.accountNo}')">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                                : ''
+                            }
+                        </td>
+                    </tr>
+                )}
+            </tbody>
         </table>
     )
 }
