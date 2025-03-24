5c7fc4f0dadfab153f7700879b351f99aeb93127
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 21:41:29 +0300

    updated

--- Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/components/AccountsTable.jsx
@@ -1,14 +1,43 @@
 'use client'
 import React from 'react'
 
-export default function AccountsTable({ x, y, initialAccounts, onAlertMe }) {
+export default function AccountsTable({ initialAccounts }) {
     return (
-        <div>
-            AccountsTable
-            x = {x}
-            y= {y}
-            
-            <button onClick={() => onAlertMe()}>Alert First Element</button>
-        </div>
+        <table id="accounts">
+            <tr>
+                <th>Profile Image</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Gender</th>
+                <th>Account No</th>
+                <th>Account Type</th>
+                <th>Balance</th>
+                <th>Email</th>
+                <th>Date Opened</th>
+                <th>Action</th>
+            </tr>
+            {initialAccounts.map(acct =>
+                <tr id={"row-" + acct.accountNo}>
+                    <td><img src={acct.profileImage} alt="Profile Image" className="profile-pic" /></td>
+                    <td>{acct.firstname}</td>
+                    <td>{acct.lastname}</td>
+                    <td>{acct.gender}</td>
+                    <td>{acct.accountNo}</td>
+                    <td>{acct.acctType}</td>
+                    <td>{acct.balance} QR</td>
+                    <td>{acct.email}</td>
+                    <td>{acct.dateOpened}</td>
+                    <td>
+                        {acct.balance >= 0 ?
+                            <button onclick="handleDeleteAccount('{acct.accountNo}')" className="btn-delete">
+                                <i className="fas fa-trash">Delete</i>
+                            </button> : ''}
+                        <button onclick="handleEditAccount('{acct.accountNo}')" className="btn-edit">
+                            <i className="fas fa-edit">Edit</i>
+                        </button>
+                    </td>
+                </tr>
+            )}
+        </table>
     )
 }
--- Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/page.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/page.jsx
@@ -1,32 +1,13 @@
-'use client'
 import { Inter } from 'next/font/google'
 import AccountsTable from './components/AccountsTable'
 const inter = Inter({ subsets: ['latin'] })
+import accountsRepo from '@/app/repo/accounts-repo'
 
-
-export default function Home() {
-  // we will fetch the data here
-  const accounts = [
-    { id: 1, name: 'Cash', type: 'Asset' },
-    { id: 2, name: 'Bank', type: 'Asset' },
-    { id: 3, name: 'Credit Card', type: 'Liability' },
-  ]
-
-  function alertFirstElement() {
-    alert(JSON.stringify(accounts[0]))
-  }
-
-  // we will get the data
-  // const accounts = 
+export default async function Home() {
+  const accounts = await accountsRepo.getAccounts('all')
   return (
     <>
-      <h1 >Accounts</h1>
-      <AccountsTable
-        initialAccounts={accounts}
-        x={1}
-        y={2}
-        onAlertMe={alertFirstElement}
-      />
+      <AccountsTable initialAccounts={accounts} />
     </>
   )
 }
