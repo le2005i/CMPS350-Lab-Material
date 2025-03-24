18dbe06fba7363051aed1e96534e7ab47e44805d
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 22:07:37 +0300

    iupdated

--- Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/components/AccountsTable.jsx
@@ -4,24 +4,40 @@ import AccountRow from './AccountRow'
 
 export default function AccountsTable({ initialAccounts }) {
     return (
-        <table id="accounts">
-            <thead>
-                <tr>
-                    <th>Profile Image</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Gender</th>
-                    <th>Account No</th>
-                    <th>Account Type</th>
-                    <th>Balance</th>
-                    <th>Email</th>
-                    <th>Date Opened</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {initialAccounts.map(acct => <AccountRow key={acct.id} acct={acct} />)}
-            </tbody>
-        </table>
+        <>
+            <label for="acctType">
+                Account Type
+            </label>
+            <select id="acctType" onchange="handleLoadAccounts(this.value)" class="filter-dropdown">
+                <option value="All">All</option>
+                <option value="Saving">Saving</option>
+                <option value="Current">Current</option>
+            </select>
+            <table id="accounts">
+                <thead>
+                    <tr>
+                        <th>Profile Image</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Gender</th>
+                        <th>Account No</th>
+                        <th>Account Type</th>
+                        <th>Balance</th>
+                        <th>Email</th>
+                        <th>Date Opened</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        initialAccounts.map(acct =>
+                            <AccountRow
+                                key={acct.id}
+                                acct={acct}
+                            />
+                        )}
+                </tbody>
+            </table>
+        </>
     )
 }
--- Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/layout.js
+++ Lab 9– Web Pages using Next.js and React/Tutorial/B02/lab08-bank-app/app/layout.js
@@ -10,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" />
       </head>
       <body>
         <NavBar />
