9206abb296fc29209e47c910ddd3ca16654fae71
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 16:10:53 +0300

    updates

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
@@ -1,33 +1,31 @@
 import React from 'react'
+import AccountRow from './AccountRow'
 
 export default function AccountsTable({ initialAccounts }) {
     return (
-        <table id="accounts">
-            <thead>
-                <tr>
-                    <th>Account No</th>
-                    <th>Account Type</th>
-                    <th>Balance</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {initialAccounts.map(acct =>
-                    <tr id="row-{acct.accountNo}">
-                        <td>{acct.accountNo}</td>
-                        <td>{acct.acctType}</td>
-                        <td>{acct.balance}</td>
-                        <td>
-                            {acct.balance == 0 ?
-                                <button onclick="handleDeleteAccount('${acct.accountNo}')">
-                                    <i class="fas fa-trash"></i>
-                                </button>
-                                : ''
-                            }
-                        </td>
+        <>
+            <label for="acctType">
+                Account Type
+            </label>
+            <select id="acctType" onchange="handleLoadAccounts(this.value)" class="filter-dropdown">
+                <option value="All">All</option>
+                <option value="Saving">Saving</option>
+                <option value="Current">Current</option>
+            </select>
+            <table id="accounts">
+                <thead>
+                    <tr>
+                        <th>Account No</th>
+                        <th>Account Type</th>
+                        <th>Balance</th>
+                        <th>Action</th>
                     </tr>
-                )}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {initialAccounts.map(acct => <AccountRow account={acct} />
+                    )}
+                </tbody>
+            </table>
+        </>
     )
 }
