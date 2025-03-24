f5e3aa88cf604e5d5a606dd0acd74aae19fe4634
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 15:56:56 +0300

    up

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function AccountsTable({ initialAccounts }) {
     return (
-        <div>I will generate the list of accounts</div>
+        <table id="accounts">
+            <tr>
+                <th>Account No</th>
+                <th>Account Type</th>
+                <th>Balance</th>
+                <th>Action</th>
+            </tr>
+            {initialAccounts.map(acct =>
+                <tr id="row-{acct.accountNo}">
+                    <td>{acct.accountNo}</td>
+                    <td>{acct.acctType}</td>
+                    <td>{acct.balance}</td>
+                    <td>
+                        {acct.balance == 0 ?
+                            <button onclick="handleDeleteAccount('${acct.accountNo}')">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                            : ''
+                        }
+                    </td>
+                </tr>
+            )}
+        </table>
     )
 }
