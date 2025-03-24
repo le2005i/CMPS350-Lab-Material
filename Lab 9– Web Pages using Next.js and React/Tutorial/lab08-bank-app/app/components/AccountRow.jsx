0d1bad04405c1ae54a8fbaeecedda1b74b8a382a
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-24 16:45:27 +0300

    updated

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountRow.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountRow.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 
-export default function AccountRow({ account }) {
+export default function AccountRow({ account, onDelete }) {
     return (
         <tr id="row-{account.accountNo}">
             <td>{account.accountNo}</td>
             <td>{account.acctType}</td>
             <td>{account.balance}</td>
             <td>
-                {account.balance == 0 ?
-                    <button>
-                        <i class="fas fa-trash"></i>
+                {account.balance >= 0 ?
+                    <button className='btn-delete' onClick={() => onDelete(account.accountNo)}>
+                        <i class="fas fa-trash">Delete</i>
                     </button>
                     : ''
                 }
+                <button className='btn-edit' >
+                    <i class="fas fa-edit">Edit</i>
+                </button>
             </td>
-        </tr>
+        </tr >
     )
 }
--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/components/AccountsTable.jsx
@@ -7,6 +7,22 @@ import AccountRow from './AccountRow'
 export default function AccountsTable({ initialAccounts }) {
     const [accounts, setAccounts] = useState(initialAccounts)
 
+    async function deleteAccount(accountNo) {
+        // create a confirmation dialog
+        if (!confirm('Are you sure you want to delete this account?')) {
+            return
+        }
+        const response = await fetch(`http://localhost:3000/api/accounts/${accountNo}`, {
+            method: 'DELETE'
+        })
+        if (response.ok) {
+            // 
+            const r = await fetch(`http://localhost:3000/api/accounts/`)
+            const newAccounts = await r.json()
+            setAccounts(newAccounts)
+        }
+    }
+
     async function handleTypeChange(type) {
         const response = await fetch(`http://localhost:3000/api/accounts?type=${type}`)
         const accounts = await response.json()
@@ -33,7 +49,7 @@ export default function AccountsTable({ initialAccounts }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {accounts.map(acct => <AccountRow account={acct} />
+                    {accounts.map(acct => <AccountRow account={acct} onDelete={deleteAccount} />
                     )}
                 </tbody>
             </table>
