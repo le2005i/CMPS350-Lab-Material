8becccc7d8ca9a55173ff80ccb15a7e1b22923fc
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-03-26 11:19:28 +0300

    up

--- Lab 9– Web Pages using Next.js and React/Tutorial/B52/lab08-bank-app/app/page.jsx
+++ Lab 9– Web Pages using Next.js and React/Tutorial/B52/lab08-bank-app/app/page.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import AccountsTable from './components/AccountsTable'
 import accountRepo from '@/app/repo/accounts-repo'
+// import accountRepo from '../app/repo/accounts-repo'
 const inter = Inter({ subsets: ['latin'] })
 
 export default async function Home() {
